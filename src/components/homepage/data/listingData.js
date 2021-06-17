544a570ad4fcd59e3de5ae6d250354b46aa56750
Donly Wilson <donly6@hotmail.com>
2021-06-16 21:26:50 -0400

    update listing's data

--- src/components/homepage/data/listingData.js
+++ src/components/homepage/data/listingData.js
@@ -1,18 +1,21 @@
 var listingData = [
   {
+    id:0,
     address: "Grand Riviere",
     city: "Castries",
     state: "SLU",
-    bedrooms: 1,
-    bathrooms: 2,
+    bedrooms: 6,
+    bathrooms: 3,
     price: 2100,
     floorSpace: 2000,
     extra: ["elevator", "gym"],
     homeType: "Apartment",
+    description: "This is a 6 bedrooms, 3 full baths single-Family Home. Located in the Central Sunset. It features 4 bonus bedrooms, with 2 baths (built to code). 1-car garage with plenty of street parking. View of Golden gate. Walking distance to Corinth Secondary School. Fantastic opportunity to move into a turn-key, move-in ready home or use as a rental investment.",
     image:
       "https://passiveincomemd.com/wp-content/uploads/2017/06/Front-Back-Building.jpg",
   },
   {
+    id:1,
     address: "Cap Estate",
     city: "Gros Islet",
     state: "SLU",
@@ -22,9 +25,11 @@ var listingData = [
     floorSpace: 22000,
     extra: ["elevator", "gym"],
     homeType: "Duplex",
+    description: "This one-of-a-kind Cap Estate property has gorgeous mid-century bones with stunning city and Bay views. The expansive multi-level home is the perfect blank slate to add your personal touches and upgrades. The home welcomes you with a foyer and dining room then leads to the mezzanine overlooking the dramatic living room with soaring beamed ceilings and a wall of windows opening to the yard with bay views. The main level also includes a kitchen, full bath and bedroom ideal as a home office. Above, another large living area boasts vaulted ceilings, a wood-burning fireplace and opens to a massive deck where views stretch from end to end. An office/art studio space opens to the deck, and two west-facing bedrooms share a bathroom. A basement with laundry, wine cellar and plenty of storage completes the layout. Ample street parking and easy access to freeways, transit and parks. There is huge potential with this special view property.",
     image: "https://q-xx.bstatic.com/images/hotel/max1024x768/100/10016411.jpg",
   },
   {
+    id:2,
     address: "Black Bay",
     city: "Vieux Fort",
     state: "SLU",
@@ -34,10 +39,12 @@ var listingData = [
     floorSpace: 10000,
     extra: ["elevator", "gym", "swimming_pool"],
     homeType: "Apartment",
+    description: "Sweeping View home from 3 levels! Fully renovated Mid-Century Home atop GG Heights with light filled gated entry & skylight. Enter the main level which offers 2 bedrooms, 1 bathroom w/skylight. The open living/dining, and kitchen w/island area with view balcony allow for entertainer's dream!",
     image:
       "https://idesign161.files.wordpress.com/2017/09/gorgeous-evening-view-of-russet-residence-1025x450.jpg?w=739",
   },
   {
+    id:3,
     address: "Lime Stone",
     city: "Dennery",
     state: "SLU",
@@ -47,10 +54,12 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "Garage"],
     homeType: "Studio",
+    description: "Panoramic Lime Stone Views will immediately greet you from the zen garden entry of this spacious remodeled home. Dramatic vaulted ceilings in the open living area make this a perfect space for entertaining or cozy nights with the family. The renovated showcase kitchen with a grand quartz island and stainless steel appliances opens to the living and dining areas and is surrounded by multiple outdoor spaces and views. Stairs down lead to the large primary bedroom with ensuite bath, second bedroom, full bath, laundry and storage room.",
     image:
       "https://images.squarespace-cdn.com/content/5abbea6fc258b4f56d09219e/1524087111985-FMCVW3XY42FBBRZEC5Z5/greenfab.jpeg?content-type=image%2Fjpeg",
   },
   {
+    id:4,
     address: "25th main street",
     city: "Laborie",
     state: "SLU",
@@ -60,10 +69,12 @@ var listingData = [
     floorSpace: 8000,
     extra: ["beach"],
     homeType: "Room",
+    description: "Welcome to this gorgeous 1 Bed 1 Bath CORNER UNIT with impressive sea view, 2 balconies and parking. Getting this room include a Heated Pool, Beach, Landscaped Courtyard with Furniture, Onsite Management. Close to Grace Cathedral, Union Square and the Street corridor - Enjoy living in the heart of the Laborie!",
     image:
       "https://rentivo.imagekit.io/cdn/tr:n-max/http://dujlrb80v4oq5.cloudfront.net/578/83959073_gw1418680.jpg",
   },
   {
+    id:5,
     address: "Beach Pavillion",
     city: "Soufriere",
     state: "SLU",
@@ -73,10 +84,12 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "swimming_pool"],
     homeType: "Hotel",
+    description: "Flooded with light, inspired by nature, and crafted with Troon Pacific's unmatched expertise, Residence 2582 demonstrates the best of modern Beach Pavillion architecture rarely seen in the heart of Soufriere. Generous terraces on each floor and an outdoor garden oasis with 72' lap pool offer enticing reasons to spend more time outside.",
     image:
       "https://cdn.villa-bali.com/cache/fullSize/villas/noku-beach-house/noku-beach-house-noku-beach-house-exquisite-villa-feature-5b8f4a6f08970.jpg",
   },
   {
+    id:6,
     address: "Beach Pavillion",
     city: "Soufriere",
     state: "SLU",
@@ -86,10 +99,12 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "swimming_pool"],
     homeType: "Hotel",
+    description: "This 2 level beautifully renovated Penthouse at the Beach Pavillion showcases sweeping views from the sea to Piton Mountain. Sophistication is found at every moment, from the prominent entry hall with sweeping stair and fireplace, to the nearly 50' long grand gallery framing Bay views.",
     image:
       "https://cdn.villa-bali.com/cache/fullSize/villas/noku-beach-house/noku-beach-house-noku-beach-house-exquisite-villa-feature-5b8f4a6f08970.jpg",
   },
   {
+    id:7,
     address: "Beach Pavillion",
     city: "Soufriere",
     state: "SLU",
@@ -99,10 +114,12 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "swimming_pool"],
     homeType: "Hotel",
+    description: "This 2 level beautifully renovated Penthouse at the Beach Pavillion showcases sweeping views from the sea to Piton Mountain. Sophistication is found at every moment, from the prominent entry hall with sweeping stair and fireplace, to the nearly 50' long grand gallery framing Bay views.",
     image:
       "https://cdn.villa-bali.com/cache/fullSize/villas/noku-beach-house/noku-beach-house-noku-beach-house-exquisite-villa-feature-5b8f4a6f08970.jpg",
   },
   {
+    id:8,
     address: "Beach Pavillion",
     city: "Soufriere",
     state: "SLU",
@@ -112,10 +129,12 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "swimming_pool"],
     homeType: "Hotel",
+    description: "This 2 level beautifully renovated Penthouse at the Beach Pavillion showcases sweeping views from the sea to Piton Mountain. Sophistication is found at every moment, from the prominent entry hall with sweeping stair and fireplace, to the nearly 50' long grand gallery framing Bay views.",
     image:
       "https://cdn.villa-bali.com/cache/fullSize/villas/noku-beach-house/noku-beach-house-noku-beach-house-exquisite-villa-feature-5b8f4a6f08970.jpg",
   },
   {
+    id:9,
     address: "Beach Pavillion",
     city: "Soufriere",
     state: "SLU",
@@ -125,6 +144,7 @@ var listingData = [
     floorSpace: 7000,
     extra: ["elevator", "swimming_pool"],
     homeType: "Hotel",
+    description: "This 2 level beautifully renovated Penthouse at the Beach Pavillion showcases sweeping views from the sea to Piton Mountain. Sophistication is found at every moment, from the prominent entry hall with sweeping stair and fireplace, to the nearly 50' long grand gallery framing Bay views.",
     image:
       "https://cdn.villa-bali.com/cache/fullSize/villas/noku-beach-house/noku-beach-house-noku-beach-house-exquisite-villa-feature-5b8f4a6f08970.jpg",
   },
